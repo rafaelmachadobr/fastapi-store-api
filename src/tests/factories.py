ff0ab6d4f887725ef629278198d1e248a619cb05
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2024-07-05 23:57:34 -0300

    feat: Add product data validation in test_schemas_return_error

---
+++ src/tests/factories.py
@@ -0,0 +1,7 @@
+def product_data():
+    return {
+        "name": "iPhone 15 Pro Max",
+        "quantity": 10,
+        "price": 8500.00,
+        "status": True,
+    }
---
+++ src/tests/schemas/test_product.py
@@ -0,0 +1,29 @@
+from uuid import UUID
+
+from pydantic import ValidationError
+import pytest
+from src.store.schemas.product import ProductIn
+from src.tests.factories import product_data
+
+
+def test_schemas_validated():
+    data = product_data()
+    product = ProductIn.model_validate(data)
+
+    assert product.name == "iPhone 15 Pro Max"
+    assert isinstance(product.id, UUID)
+
+
+def test_schemas_return_error():
+    data = {"name": "iPhone 15 Pro Max", "quantity": 10, "price": 8500.00}
+
+    with pytest.raises(ValidationError) as err:
+        ProductIn.model_validate(data)
+
+    assert err.value.errors()[0] == {
+        "type": "missing",
+        "loc": ("status",),
+        "msg": "Field required",
+        "input": {"name": "iPhone 15 Pro Max", "quantity": 10, "price": 8500.0},
+        "url": "https://errors.pydantic.dev/2.8/v/missing",
+    }
